fd39e137cb515bbbd48591b082a7f2b77c1d5c80
GennadiySerikov <serikov.gen@yandex.ru>
2021-10-14 01:09:12 +0300

    feat: add binary tree empty domain without implemenetation

---
+++ lesson4/BinaryTree/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿namespace lesson4.BinaryTree
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+
+        public override bool Equals(object obj) {
+            var node = obj as TreeNode;
+
+            if (node == null)
+                return false;
+
+            return node.Value == Value;
+        }
+
+        public override int GetHashCode() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
